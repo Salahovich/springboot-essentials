275fa816af312bc3c97356c253c3251b21d4b988
Muhammad Salah Abdallah <84870808+Salahovich@users.noreply.github.com>
2023-11-08 13:24:31 +0200

    Adding Redis Caching service to the Main Service Layer

--- src/main/java/giza/example/springbootessentials/Services/InstructorService.java
+++ src/main/java/giza/example/springbootessentials/Services/InstructorService.java
@@ -1,9 +1,11 @@
 package giza.example.springbootessentials.Services;
 
+import giza.example.springbootessentials.Caching.RedisService;
 import giza.example.springbootessentials.Exceptions.InstructorNotFoundException;
 import giza.example.springbootessentials.Models.Instructor;
 import giza.example.springbootessentials.Repositories.JPA.InstructorRepositoryJpa;
 import giza.example.springbootessentials.Validations.InstructorValidation;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
+@Log
 public class InstructorService {
 
     @Autowired
@@ -20,12 +23,23 @@ public class InstructorService {
     @Autowired
     private InstructorValidation validation;
 
+    @Autowired
+    private RedisService redisService;
+
     public Iterable<Instructor> findAll(){
         return repository.findAll();
     }
 
     public Instructor findInstructorById(UUID uuid){
-        return repository.findById(uuid).orElseThrow(InstructorNotFoundException::new);
+        if(redisService.hasKey(uuid.toString())){
+            log.info("Instructor Cache HIT");
+            return  (Instructor) redisService.getValueFromRedis(uuid.toString()).get();
+        }else{
+            Instructor instructor = repository.findById(uuid).orElseThrow(InstructorNotFoundException::new);
+            redisService.setValueInRedis(instructor.getId().toString(), instructor);
+            log.info("Instructor Cache MISS");
+            return instructor;
+        }
     }
 
     public List<Map<String, Object>> findInstructorsWithTheirCourses(){
@@ -45,12 +59,23 @@ public class InstructorService {
     }
 
     public Instructor updateInstructor(Instructor instructor){
-        return saveInstructor(instructor);
+        if(findInstructorById(instructor.getId()) == null)
+            throw new InstructorNotFoundException();
+
+        instructor = saveInstructor(instructor);
+        if(redisService.hasKey(instructor.getId().toString())){
+            redisService.setValueInRedis(instructor.getId().toString(), instructor);
+            log.info("Updating Cache for instructor");
+        }
+
+        return instructor;
     }
 
     public void deleteInstructorById(UUID id){
         repository.findById(id).orElseThrow(InstructorNotFoundException::new);
         repository.deleteById(id);
+        redisService.deleteKey(id.toString());
+        log.info("Deleting Cache for instructor");
     }
 
     protected void checkEmail(String email){
--- src/main/java/giza/example/springbootessentials/Services/StudentService.java
+++ src/main/java/giza/example/springbootessentials/Services/StudentService.java
@@ -1,9 +1,11 @@
 package giza.example.springbootessentials.Services;
 
+import giza.example.springbootessentials.Caching.RedisService;
 import giza.example.springbootessentials.Exceptions.StudentNotFoundException;
 import giza.example.springbootessentials.Models.Student;
 import giza.example.springbootessentials.Repositories.JPA.StudentRepositoryJpa;
 import giza.example.springbootessentials.Validations.StudentValidation;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
+@Log
 public class StudentService {
 
     @Autowired
@@ -20,12 +23,25 @@ public class StudentService {
     @Autowired
     private StudentRepositoryJpa repository;
 
+    @Autowired
+    private RedisService redisService;
+
     public Iterable<Student> findAll(){
         return repository.findAll();
     }
 
     public Student findStudentById(UUID uuid){
-        return repository.findById(uuid).orElseThrow(StudentNotFoundException::new);
+        var cachedStudent = redisService.getValueFromRedis(uuid.toString());
+        if(cachedStudent.isPresent()){
+            log.info("Student Cache HIT");
+            return (Student) cachedStudent.get();
+        }
+        else{
+            Student theStudent = repository.findById(uuid).orElseThrow(StudentNotFoundException::new);
+            redisService.setValueInRedis(uuid.toString(), theStudent);
+            log.info("Student Cache MISS");
+            return theStudent;
+        }
     }
     public List<Map<String, Object>> findStudentsEnrolledInIntermediateCourses(){
         return repository.findStudentsEnrolledInIntermediateCourses();
@@ -40,11 +56,22 @@ public class StudentService {
         return repository.save(student);
     }
     public Student updateStudent(Student student){
-        return saveStudent(student);
+        if(findStudentById(student.getId()) == null)
+            throw new StudentNotFoundException();
+        student = saveStudent(student);
+
+        if(redisService.hasKey(student.getId().toString())){
+            redisService.setValueInRedis(student.getId().toString(), student);
+            log.info("Updating student in Cache");
+        }
+
+        return student;
     }
     public void deleteStudentById(UUID uuid){
         repository.findById(uuid).orElseThrow(StudentNotFoundException::new);
         repository.deleteById(uuid);
+        redisService.deleteKey(uuid.toString());
+        log.info("Deleting Student from Cache");
     }
 
     // wrapper validation methods
